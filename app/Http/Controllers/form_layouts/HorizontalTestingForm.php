b1656c168111cf2a0dddcb2d3bf18aa624317710
unknown <ammarnaim50@gmail.com>
2023-11-24 00:25:21 +0700

    fixing chart and adding pop up

--- app/Http/Controllers/form_layouts/HorizontalTestingForm.php
+++ app/Http/Controllers/form_layouts/HorizontalTestingForm.php
@@ -8,14 +8,17 @@ use Illuminate\Http\Request;
 
 class HorizontalTestingForm extends Controller
 {
-  public $raph;
-  public function __construct() {
-    $this->raph = new RolesAndPermissionsHelper();
+  public function index(){
+    return view('content.form-layout.form-layouts-horizontaltesting');
   }
-    public function index()
-    {
-        $data = $this->raph->linkPermissions();
-        return response()->json($data);
-        // return view('content.form-layout.form-layouts-horizontaltesting');
-    }
+  //public $raph;
+  //public function __construct() {
+  //  $this->raph = new RolesAndPermissionsHelper();
+  //}
+  //  public function index()
+  //  {
+  //      $data = $this->raph->linkPermissions();
+  //      return response()->json($data);
+        
+  //  }
 }
