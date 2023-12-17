941088d10cfc35294d47348533f8b326a9454986
muhridhaagam <muhridhaagam@gmail.com>
2023-12-18 01:21:01 +0700

    feat: approval 3 fix

---
+++ resources/views/content/permit_to_work/__approval_three.blade.php
@@ -0,0 +1,48 @@
+<div class="col">
+    {{-- <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Forms/</span>Permit to Work</h4> --}}
+    <!-- Basic Layout -->
+    <div class="card mb-4">
+        <div class="card-header">
+            <div class="text-center">
+                <h3>Permit To Work Request</h3>
+                <h5>Approval 3</h5>
+            </div>
+
+            <form id="formAccountSettings" method="POST" action="{{ route('permit_to_work.store_header') }}">
+                @csrf
+                <div class="mt-2 d-flex justify-content-end">
+                    <button type="submit" class="btn btn-outline-secondary me-2">Save</button>
+                    <button id="submit_permit_to_work" class="btn btn-primary me-2 disabled">Submit</button>
+                </div>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                {{-- Closed Out PA --}}
+                <h3 class="form-header">Closed Out PA</h3>
+                <div class="mb-3 col-md-12">
+                    {{-- <input type="text" class="form-control" id="directspv" name="directspv" /> --}}
+                    <select id="closed_out_pa" class="form-select permit_to_work" name="closed_out_pa"
+                        aria-label="closed_out_pa" data-placeholder="Select Name">
+                        <option>Harold Carter</option>
+                    </select>
+                </div>
+                {{-- Closed Out AA --}}
+                <h3 class="form-header mt-5">Closed Out AA</h3>
+                <div class="mb-3 col-md-12">
+                    {{-- <input type="text" class="form-control" id="directspv" name="directspv" /> --}}
+                    <select id="closed_out_aa" class="form-select permit_to_work" name="closed_out_aa"
+                        aria-label="closed_out_aa" data-placeholder="Select Name">
+                        <option>Harold Carter</option>
+                    </select>
+                </div>
+                </form>
+                <div class="mt-2 d-flex justify-content-end">
+                    <button class="btn btn-primary me-2" onclick="stepper1.previous()">Previous</button>
+                    <button class="btn btn-primary me-2" onclick="stepper1.next()">Next</button>
+                </div>
+            </div>
+
+        </div>
+        <!-- /Account -->
+    </div>
+</div>
